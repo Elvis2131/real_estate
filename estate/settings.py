9a0787e440b83c4e4b24e053ae34b3a1aad7715c
elvis <larteyelvis@outlook.com>
2020-12-30 15:29:01 +0000

    testing

--- estate/settings.py
+++ estate/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
